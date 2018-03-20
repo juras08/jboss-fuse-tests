3196f990417b041e5e30f6bf6ef50fa88fa8f413
Juri Solovjov <jsolovjo@redhat.com>
2018-03-20 14:20:36 +0100

    [browser] Open hawtio and do some simple actions

---
+++ fuse-browser/src/test/java/com/jboss/fuse/test/practice/browser/ExBrowserTest.java
@@ -0,0 +1,55 @@
+package com.jboss.fuse.test.practice.browser;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.jboss.fuse.qa.fafram8.resource.Fafram;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class ExBrowserTest {
+//	@ClassRule
+//	public static Fafram fafram = new Fafram();
+
+	@Before
+	public void timeout() {
+//		try {
+//			Thread.sleep(5000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+	}
+
+	@Test
+	public void exBrowserTest() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://localhost:8181/hawtio");
+
+		try {
+			Thread.sleep(10000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("admin");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("admin");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		driver.findElement(By.cssSelector(".btn.btn-success")).click();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		driver.findElement(By.xpath("//a[text()='Dashboard']")).click();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		driver.findElement(By.xpath("//input[@class='search-query ng-pristine ng-valid']")).sendKeys("Process");
+	}
+}
