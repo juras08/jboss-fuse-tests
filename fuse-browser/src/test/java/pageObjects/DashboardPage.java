946713b57a1ede6945e2ce112c5aac9a71980bdf
Juri Solovjov <jsolovjo@redhat.com>
2018-03-21 13:19:54 +0100

    [pageobj] Page Objects made

---
+++ fuse-browser/src/test/java/pageObjects/DashboardPage.java
@@ -0,0 +1,10 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class DashboardPage {
+	private WebDriver driver;
+	public DashboardPage(WebDriver driver) {
+		this.driver = driver;
+	}
+}
